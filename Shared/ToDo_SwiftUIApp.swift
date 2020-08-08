ce9b05f0bd94b22989cc78905d895036c7b85f86
hellosaumil <6513922+hellosaumil@users.noreply.github.com>
2020-08-08 12:04:21 -0700

    App from Scratch with SwiftUI 2 LifeCycle

---
+++ Shared/ToDo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Saumil Shah on 8/8/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
